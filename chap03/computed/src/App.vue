dd03e59a52d80882fd77e2d2e0c8c86136a0f10d
yukiko bass <springcomesagain129@gmail.com>
2023-04-18 18:05:18 +0900

    chap03-3.1

---
+++ chap03/computed/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const radiusInit = Math.round(Math.random() * 10)
+
+const PI = ref(3.14)
+
+const raduis = ref(radiusInit)
+
+const area = computed((): number => {
+  return raduis.value + raduis.value * PI.value
+})
+
+setInterval((): void => {
+  raduis.value = Math.round(Math.random() * 10)
+}, 1000)
+</script>
+
+<template>
+  <p>半径{{ raduis }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+</template>
