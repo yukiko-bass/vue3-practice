28c59a1440f5476ad668df8cd3ea3057ba2b1653
yukiko bass <springcomesagain129@gmail.com>
2023-06-16 18:25:01 +0900

    chap 5.1.2

---
+++ chap05/directive-oneway/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputNameBind = ref('どな')
+const inputNameOn = ref('ななち')
+const onInputName = (event: Event): void => {
+  const element = event.target as HTMLInputElement
+  inputNameOn.value = element.value
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputNameBind" />
+  </section>
+  <br />
+  <section>
+    <input type="text" v-on:input="onInputName" />
+    <p>{{ inputNameOn }}</p>
+  </section>
+</template>
