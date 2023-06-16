2e73b2353cc43ca3b135f6f22b4a3e886014918d
yukiko bass <springcomesagain129@gmail.com>
2023-06-16 17:40:28 +0900

    chap 4.2.7

---
+++ chap04/directive-on-modifiers/src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const msg = ref('まだです')
+const onEnterKey = (): void => {
+  msg.value = 'エンターキーが押された'
+}
+const onRightButtonClick = (): void => {
+  msg.value = '右クリックが押された'
+}
+const onShiftClick = (): void => {
+  msg.value = 'シフトを押しながらクリックされた'
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <input type="text" v-on:keydown.enter="onEnterKey" /><br />
+  <button v-on:-click.right="onRightButtonClick">右クリック</button><br />
+  <button v-on:click.shift="onShiftClick">シフトを押しながらクリック</button>
+</template>
