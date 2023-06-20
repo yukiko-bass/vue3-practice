87bd0643be924566b0f685cc8e1c510ea3c263f5
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 17:51:21 +0900

    chap 6.2.5

---
+++ chap06/directive-loop-arrayobj/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const cocktailDataListInit: Cocktail[] = [
+  { id: 2345, name: 'ホワイトレディ', price: 1200 },
+  { id: 4412, name: 'ブルーハワイ', price: 1500 },
+  { id: 6792, name: 'ニューヨーク', price: 1100 },
+  { id: 8429, name: 'マティーニ', price: 1500 }
+]
+
+interface Cocktail {
+  id: number
+  name: string
+  price: number
+}
+const cocktailDataList = ref(cocktailDataListInit)
+</script>
+
+<template>
+  <ul>
+    <li v-for="cocktailItem in cocktailDataList" v-bind:key="cocktailItem.id">
+      {{ cocktailItem.name }} の値段は {{ cocktailItem.price }}円
+    </li>
+  </ul>
+</template>
