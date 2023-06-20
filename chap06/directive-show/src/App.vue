6a8767cd7b5ecfd8256cb8a061f1ab4ba2a0ecd5
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 16:30:41 +0900

    chap 6.1.4

---
+++ chap06/directive-show/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const number = ref(80)
+const showOrNot = computed((): boolean => {
+  let showOrNot = false
+  const rand = Math.round(Math.random() * 100)
+  if (rand >= 50) {
+    showOrNot = true
+  }
+  return showOrNot
+})
+</script>
+
+<template>
+  <section>
+    v-if を利用
+    <p v-if="showOrNot">条件に合致したので表示</p>
+  </section>
+  <br>
+  <section>
+    v-show を利用
+    <p v-show="showOrNot">条件に合致したので表示</p>
+  </section>
+  
+</template>
