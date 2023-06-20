af3f40e344507628a49a6bd934ddc4b852b337aa
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 18:08:57 +0900

    chap 6.2.7

---
+++ chap06/directive-loop-range/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul>
+    <li v-for="r in 5" v-bind:key="r">半径{{ r }} の円の円周：{{ 2 * r * 3.14 }}</li>
+  </ul>
+
+  <br>
+  <section>
+    <select>
+      <option v-for="year in 60" v-bind:key="year" v-bind:value="year + 1965">{{ year + 1965 }}</option>
+    </select>
+  </section>
+</template>
