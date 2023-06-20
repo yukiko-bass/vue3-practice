7413a68b16b076444e76ec6b2dba2ce803c68703
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 15:42:49 +0900

    chap 6.1.1

---
+++ chap06/directive-conditional-basic/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const number = ref(80)
+const showOrNot = computed((): boolean => {
+  let showOrNot = false
+  const rand = Math.round(Math.random() * 100)
+  if (rand >= 50) {
+    showOrNot = true
+  }
+  return showOrNot
+})
+</script>
+
+<template>
+  <p v-if="number >= 50">条件に合致したので表示①</p><br>
+  <p v-if="Math.round(Math.random() * 100) >= 50">条件に合致したので表示②</p><br>
+  <p v-if="showOrNot">条件に合致したので表示③</p>
+</template>
