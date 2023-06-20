279862586fe55b03d7e4f142e9931703cf0610d5
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 16:44:25 +0900

    chap 6.2.1

---
+++ chap06/directive-loop-basic/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const cocktailListInit: string[] = ['ホワイトレディ', 'ブルーハワイ', 'ニューヨーク']
+const cocktailList = ref(cocktailListInit)
+</script>
+
+<template>
+  <ul>
+    <li v-for="cocktailName in cocktailList" v-bind:key="cocktailName">{{ cocktailName }}</li>
+  </ul>
+  <ul>
+    <li v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
+      {{ cocktailName }}(インデックス{{ index }})
+    </li>
+  </ul>
+</template>
