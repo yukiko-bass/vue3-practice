b8c6b007a64e4f532a32e2bc0daade4dbe2af6e7
yukiko bass <springcomesagain129@gmail.com>
2023-06-20 18:24:14 +0900

    chap 6.3.1

---
+++ chap06/listchange-filter/src/App.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+const cocktailDataListInit: Cocktail[] = [
+  { id: 2345, name: 'ホワイトレディ', price: 1200 },
+  { id: 4412, name: 'ブルーハワイ', price: 1500 },
+  { id: 6792, name: 'ニューヨーク', price: 1100 },
+  { id: 8429, name: 'マティーニ', price: 1500 }
+]
+
+const cocktailDataList = ref(cocktailDataListInit)
+
+const cocktail1500 = computed((): Cocktail[] => {
+  const newList = cocktailDataList.value.filter((cocktailItem: Cocktail): boolean => {
+    return cocktailItem.price == 1500
+  })
+  return newList
+})
+
+interface Cocktail {
+  id: number
+  name: string
+  price: number
+}
+</script>
+
+<template>
+  <section>
+    全てのカクテルリスト
+    <ul>
+      <li v-for="cocktailItem in cocktailDataList" v-bind:key="cocktailItem.id">
+        {{ cocktailItem.name }} の値段は {{ cocktailItem.price }}円
+      </li>
+    </ul>
+  </section>
+  <br />
+  <section>
+    値段が1500円のカクテルリスト
+    <ul>
+      <li v-for="cocktailItem in cocktail1500" v-bind:key="'cocktail1500' + cocktailItem.id">
+        {{ cocktailItem.name }} の値段は {{ cocktailItem.price }}円
+      </li>
+    </ul>
+  </section>
+</template>
