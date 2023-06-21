6107940c3b4aa359c482f743e520d3eb36054a63
yukiko bass <springcomesagain129@gmail.com>
2023-06-21 12:10:35 +0900

    chap 7.1.1

---
+++ chap07/change-cocktail/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const cocktailDataListInit = new Map<number, Cocktail>()
+cocktailDataListInit.set(1, { id: 1, name: 'ホワイトレディ', price: 1200 })
+cocktailDataListInit.set(2, { id: 2, name: 'ブルーハワイ', price: 1500 })
+cocktailDataListInit.set(3, { id: 3, name: 'ニューヨーク', price: 1100 })
+cocktailDataListInit.set(4, { id: 4, name: 'マティーニ', price: 1500 })
+
+const cocktailNo = ref(1)
+
+const priceMsg = computed((): string => {
+  const cocktail = cocktailDataListInit.get(cocktailNo.value)
+  let msg = '該当カクテルはありません。'
+  if (cocktail != undefined) {
+    msg = `該当するカクテルは${cocktail.name}で、価格は${cocktail.price}円です。`
+  }
+  return msg
+})
+
+setInterval((): void => {
+  cocktailNo.value = Math.round(Math.random() * 3) + 1
+}, 1000)
+
+interface Cocktail {
+  id: number
+  name: string
+  price: number
+}
+const cocktailDataList = ref(cocktailDataListInit)
+</script>
+
+<template>
+  <p>現在のカクテル番号：{{ cocktailNo }}</p>
+  <p>{{ priceMsg }}</p>
+</template>
