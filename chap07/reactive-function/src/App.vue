15d9b4c90f438851fa918d3e36b341c34e17f923
yukiko bass <springcomesagain129@gmail.com>
2023-06-21 16:02:38 +0900

    chap 7.3.2

---
+++ chap07/reactive-function/src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent, reactive, computed } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    const data = reactive({
+      PI: 3.14,
+      radius: Math.round(Math.random() * 10)
+    })
+
+    const area = computed((): number => {
+      return data.radius * data.radius * data.PI
+    })
+
+    setInterval((): void => {
+      data.radius = Math.round(Math.random() * 10)
+    }, 1000)
+
+    return {
+      data,
+      area
+    }
+  }
+})
+</script>
+
+<template>
+  <p>半径{{ data.radius }} の円の面積を円周率{{ data.PI }}で計算すると、{{ area }}</p>
+</template>
