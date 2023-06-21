d4b17cf9fc780674ac42bfcbff57d7aeac276826
yukiko bass <springcomesagain129@gmail.com>
2023-06-21 17:26:55 +0900

    chap 8.3.3

---
+++ chap08/components-props-bind/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneInfo from './components/OneInfo.vue'
+
+const propsTitle = ref('発生した乱数')
+const rand = Math.round(Math.random() * 10)
+const propsContent = ref(rand)
+</script>
+
+<template>
+  <h1>Props基礎</h1>
+  <section>
+    <h2>テンプレート変数を利用</h2>
+    <OneInfo v-bind:title="propsTitle" v-bind:content="propsContent" />
+  </section>
+</template>
+
+<style>
+.section {
+  border: blue 1px solid;
+  margin: 10px;
+}
+</style>
