db7b96efb830919995591205c87c3433ada3c3fb
yukiko bass <springcomesagain129@gmail.com>
2023-06-22 18:07:04 +0900

    chap 9.1.3

---
+++ chap09/slot-fallback/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue'
+import { ref } from 'vue'
+
+const taro = ref('田中太郎')
+const jiro = ref('鈴木二郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slot の利用</h2>
+    <OneSection v-bind:name="taro">
+      <p>連絡がつきません</p>
+    </OneSection>
+    <OneSection v-bind:name="jiro"></OneSection>
+  </section>
+</template>
