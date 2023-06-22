2665adf124b59c93278a1d438baabd2cb484d683
yukiko bass <springcomesagain129@gmail.com>
2023-06-22 17:17:22 +0900

    chap 8.6

---
+++ chap08/components-provinje/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { reactive, provide } from 'vue'
+import BaseSection from './components/BaseSection.vue'
+import type { Member } from './interfaces'
+
+const memberList = new Map<number, Member>()
+memberList.set(33456, {
+  id: 33456,
+  name: '田中太郎',
+  email: 'bow@example.com',
+  points: 35,
+  note: '初回入会特典あり'
+})
+memberList.set(47783, { id: 47783, name: '鈴木二郎', email: 'mue@example.com', points: 53 })
+provide('memberList', reactive(memberList))
+</script>
+
+<template>
+  <BaseSection />
+</template>
---
+++ chap08/components-provinje/src/components/OneMember.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { computed, inject } from 'vue'
+import type { Member } from '../interfaces'
+
+interface Props {
+  id: number
+}
+const props = defineProps<Props>()
+
+const memberList = inject('memberList') as Map<number, Member>
+const member = computed((): Member => {
+  return memberList.get(props.id) as Member
+})
+
+const localNote = computed((): string => {
+  let localNote = member.value.note
+  if (localNote == undefined) {
+    localNote = '--'
+  }
+  return localNote
+})
+</script>
+
+<template>
+  <section class="box">
+    <h4>{{ member.name }}さんの情報</h4>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ id }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ member.email }}</dd>
+      <dt>保有ポイント</dt>
+      <dd><input type="number" v-model.number="member.points" /></dd>
+      <dt>備考</dt>
+      <dd>{{ localNote }}</dd>
+    </dl>
+    <br />
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
