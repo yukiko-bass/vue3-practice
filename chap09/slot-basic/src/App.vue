80a144546b3fed50efbff5dd3bf3fbff27b8b626
yukiko bass <springcomesagain129@gmail.com>
2023-06-22 17:50:11 +0900

    chap 9.1.2

---
+++ chap09/slot-basic/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue'
+import { ref } from 'vue'
+
+const taro = ref('田中太郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slot の利用</h2>
+    <OneSection v-bind:name="taro">
+      <p>連絡がつきません</p>
+    </OneSection>
+  </section>
+</template>
