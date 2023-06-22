5c2bedecec3bccfbd0d08992604c6e9718efcc1f
yukiko bass <springcomesagain129@gmail.com>
2023-06-22 11:47:16 +0900

    chap 8.4.3

---
+++ chap08/components-props-indepth2/src/components/OneMember.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+
+interface Props {
+  id: number
+  name: string
+  email: string
+  points: number
+  note?: string
+}
+
+const props = withDefaults(defineProps<Props>(), { note: '--' })
+
+const localPoints = ref(props.points)
+const pointUp = (): void => {
+  localPoints.value++
+}
+</script>
+
+<template>
+  <section class="box">
+    <h4>{{ name }}さんの情報</h4>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ id }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ email }}</dd>
+      <dt>保有ポイント</dt>
+      <dd>{{ localPoints }}</dd>
+      <dt>備考</dt>
+      <dd>{{ note }}</dd>
+    </dl>
+    <br />
+    <button v-on:click="pointUp">ポイント加算</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
