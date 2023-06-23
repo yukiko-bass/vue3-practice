5ec3919be815faac9df79dd5c9807f97142bd4bc
yukiko bass <springcomesagain129@gmail.com>
2023-06-23 10:42:49 +0900

    chap 9.3.1

---
+++ chap09/slot-scoped/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue'
+</script>
+
+<template>
+  <section>
+    <OneSection>
+      <template v-slot:default="slotProps">
+        <dl>
+          <dt>名前</dt>
+          <dd>{{ slotProps.memberInfo.name }}</dd>
+          <dt>状況</dt>
+          <dd>{{ slotProps.memberInfo.state }}</dd>
+        </dl>
+      </template>
+    </OneSection>
+  </section>
+</template>
---
+++ chap09/slot-scoped/src/components/OneSection.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const memberInfo = reactive({
+  name: '田中太郎',
+  state: '問題ありません'
+})
+</script>
+
+<template>
+  <section class="box">
+    <slot v-bind:memberInfo="memberInfo">
+      <h1>{{ memberInfo.name }}さんの状況</h1>
+      <p>{{ memberInfo.state }}</p>
+    </slot>
+  </section>
+</template>
+
+<style>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
