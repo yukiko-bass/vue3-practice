4db85bce9ee77f0b8ccad720c4a90077935281a1
yukiko bass <springcomesagain129@gmail.com>
2023-06-23 12:13:22 +0900

    chap 9.4

---
+++ chap09/components-dynamic/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { KeepAlive, ref } from 'vue'
+import Input from './components/Input.vue'
+import Radio from './components/Radio.vue'
+import Select from './components/Select.vue'
+
+const currentComp = ref(Input)
+const currentCompName = ref('Input')
+
+const compList = [Input, Radio, Select]
+const compNameList: string[] = ['Input', 'Radio', 'Select']
+let currentCompIndex = 0
+const switchComp = (): void => {
+  currentCompIndex++
+  if (currentCompIndex >= 3) {
+    currentCompIndex = 0
+  }
+  currentComp.value = compList[currentCompIndex]
+  currentCompName.value = compNameList[currentCompIndex]
+}
+</script>
+
+<template>
+  <p>コンポーネント名：{{ currentCompName }}</p>
+  <KeepAlive>
+    <component v-bind:is="currentComp" />
+  </KeepAlive>
+  <br>
+  <button v-on:click="switchComp">切り替え</button>
+</template>
