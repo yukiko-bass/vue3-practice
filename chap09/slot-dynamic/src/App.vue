f10eb025da1df7a24e01807e79665c1b5c1f1ec0
yukiko bass <springcomesagain129@gmail.com>
2023-06-23 10:02:52 +0900

    chap 9.1.5

---
+++ chap09/slot-dynamic/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import OneSection from './components/OneSection.vue'
+import { ref } from 'vue'
+
+const taroProblemsInit: string[] = ['電話が通じません。', '留守です。']
+const taro = ref('田中太郎')
+const taroProblems = ref(taroProblemsInit)
+const jiro = ref('鈴木二郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slot の利用</h2>
+    <OneSection v-bind:name="taro">
+      <ul>
+        <li v-for="problem in taroProblems" v-bind:key="problem">
+          {{ problem }}
+        </li>
+      </ul>
+    </OneSection>
+    <OneSection v-bind:name="jiro"></OneSection>
+  </section>
+</template>
