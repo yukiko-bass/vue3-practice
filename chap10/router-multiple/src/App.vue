f2f70a509749f11c2cc5c947cb793fbe68ad02ad
yukiko bass <springcomesagain129@gmail.com>
2023-06-23 18:35:33 +0900

    chap 10.5.4

---
+++ chap10/router-multiple/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<template>
+  <header><h1>マルチビューサンプル</h1></header>
+  <main>
+    <RouterView />
+  </main>
+  <section id="sub">
+    <RouterView name="sub" />
+  </section>
+</template>
+
---
+++ chap10/router-multiple/src/views/Sub.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>サブコンテンツ</p>
+</template>
