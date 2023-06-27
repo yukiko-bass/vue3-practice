835a9dd1528fd3f0694a6b22e4bbe27c862e2411
yukiko bass <springcomesagain129@gmail.com>
2023-06-27 10:30:23 +0900

    chap 12.3.9

---
+++ chap12/async-web-axios/src/stores/weather.ts
@@ -0,0 +1,67 @@
+import axios from 'axios'
+import { defineStore } from 'pinia'
+
+export interface City {
+  name: string
+  q: string
+}
+
+interface State {
+  cityList: Map<string, City>
+  selectedCity: City
+  isLoading: boolean
+  weatherDescription: string
+}
+
+export const useWeatherStore = defineStore({
+  id: 'weather',
+  state: (): State => {
+    return {
+      cityList: new Map<string, City>(),
+      selectedCity: {
+        name: '',
+        q: ''
+      },
+      isLoading: true,
+      weatherDescription: ''
+    }
+  },
+  getters: {},
+  actions: {
+    prepareCityList() {
+      this.cityList.set('Sapporo', {
+        name: '札幌',
+        q: 'Sapporo'
+      })
+      this.cityList.set('Tokyo', {
+        name: '東京',
+        q: 'Tokyo'
+      })
+      this.cityList.set('Osaka', {
+        name: '大阪',
+        q: 'Osaka'
+      })
+    },
+    async recieveWeatherInfo(id: string) {
+      this.selectedCity = this.cityList.get(id) as City
+      const weatherInfoUrl = 'https://api.openweathermap.org/data/2.5/weather'
+      const params: {
+        lang: string
+        q: string
+        appId: string
+      } = {
+        lang: 'ja',
+        q: this.selectedCity.q,
+        appId: 'b55a990b935c8b58a45cb27323f07cd7'
+      }
+      const queryParams = new URLSearchParams(params)
+      const urlFull = `${weatherInfoUrl}?${queryParams}`
+      const response = await axios.get(urlFull)
+      const weatherInfoJSON = await response.data
+      const weatherArray = weatherInfoJSON.weather
+      const weather = weatherArray[0]
+      this.weatherDescription = weather.description
+      this.isLoading = false
+    }
+  }
+})
